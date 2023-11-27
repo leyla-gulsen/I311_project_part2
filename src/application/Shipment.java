88849721cbd08ecb302f3f6a4aab973659a3e9df
krgrohe <krgrohe@iulb-if0006-09.ads.iu.edu>
2023-11-27 12:14:03 -0500

    Shipment class updates

--- src/application/Shipment.java
+++ src/application/Shipment.java
@@ -1,10 +1,48 @@
 package application;
 
+import java.util.Date;
+
+
+/**
+ * A Thneed represents a single product to be ordered in the Thneed order system.
+ * Thneeds were introduced in Dr. Seuss books. They are highly versatile 
+ * knitted fabrics that have a size and color.
+ */
 public class Shipment {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String size;
+	private String color;
+	private int quantity;
+	private Date shipdate;
+	/**
+	 * Constructs a new Thneed
+	 * @param size the size of the Thneed ("small", "medium", or "large")
+	 * @param color the color of the Thneed
+	 */
+	public Shipment(String size, String color, int quantity, Date shipdate) {
+		super();
+		this.size = size;
+		this.color = color;
+		this.quantity = quantity;
+		this.shipdate = shipdate;
 	}
+	
+	/** Getters **/
 
+	public String getSize() {
+		return size;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public Date getShipdate() {
+		return shipdate;
+	}
+	
 }
