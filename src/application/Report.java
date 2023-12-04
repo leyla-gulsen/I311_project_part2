2a9bb4484631e27ce85cd3532fa7332db7b09e5e
lsgulsen <lsgulsen@iu.edu>
2023-12-04 12:40:00 -0500

    Created general base of Report class

---
+++ src/application/Report.java
@@ -0,0 +1,33 @@
+package application;
+
+/*
+ * Repor class
+ * The program should also include a feature to generate reports that 
+ * indicate how quickly orders are filled, which items are most/least popular, 
+ * which items get back ordered, and which customers order the most product. 
+ * The reports should essentially help the business plan their inventory and 
+ * marketing better in the future based on the past programmer roles
+ */
+
+public class Report {
+	
+	public void ordersFilledReport(List<Order> orders) {
+		
+	}
+	
+	public void popularReport(List<order> orders) {
+		
+	}
+	
+	public void leastPopularReport(List<order> orders) {
+		
+	}
+	
+	public void topCustomerReport(List<customer> customers) {
+		
+	}
+	
+	public void backorderedItemsReport(List<order> orders) {
+		
+	}
+}
