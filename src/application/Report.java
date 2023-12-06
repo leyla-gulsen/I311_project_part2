e21f95f1ec315b3491511c026401a95d6b7d80c8
lsgulsen <lsgulsen@iu.edu>
2023-12-06 00:15:22 -0500

    Created topCustomerReport method in Reports

--- src/application/Report.java
+++ src/application/Report.java
@@ -1,7 +1,7 @@
 package application;
 
 /*
- * Repor class
+ * Report class
  * The program should also include a feature to generate reports that 
  * indicate how quickly orders are filled, which items are most/least popular, 
  * which items get back ordered, and which customers order the most product. 
@@ -23,8 +23,33 @@ public class Report {
 		
 	}
 	
-	public void topCustomerReport(List<customer> customers) {
-		
+	public void topCustomerReport() {
+	    if (!orders.isEmpty()) {
+	        Customer topCustomer = null;
+	        int maxOrders = 0;
+
+	        for (int i = 0; i < orders.size(); i++) {
+	            int orderCount = 0;
+	            Customer currentCustomer = orders.get(i).getCustomer();
+
+	            for (int j = 0; j < orders.size(); j++) {
+	                if (orders.get(j).getCustomer().equals(currentCustomer)) {
+	                    orderCount++;
+	                }
+	            }
+
+	            if (orderCount > maxOrders) {
+	                maxOrders = orderCount;
+	                topCustomer = currentCustomer;
+	            }
+	        }
+
+	        if (topCustomer != null) {
+	            topCustomerField.setText(topCustomer.getName());
+	        } else {
+	            topCustomerField.setText("No orders placed yet.");
+	        }
+	    }
 	}
 	
 	public void backorderedItemsReport(List<order> orders) {
